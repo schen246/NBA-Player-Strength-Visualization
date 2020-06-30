3f8cb63d9a33b68cbcbebb8f32272529a6e7ef6d
Sai Chen <chensai0811@gmail.com>
2020-06-29 23:56:11 -0400

    full functionality of NBA player strength visualization

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,8 @@ import { AutoComplete, Input, Icon } from 'antd';
 import nba from '../nba-client';
 import { PROFILE_PIC_URL_PREFIX } from '../constants';
 
-const Option = AutoComplete.Option;
+const AAA = AutoComplete.Option;
+// const {Option} = AutoComplete;
 
 class SearchBar extends Component {
    state = {
@@ -26,16 +27,16 @@ class SearchBar extends Component {
 
    render() {
        const { dataSource } = this.state;
-       const options = dataSource.map((player) => (
-           <Option key={player.playerId} value={player.fullName} className="player-option">
+       const aaa = dataSource.map((player) => (
+           <AAA key={player.playerId} value={player.fullName} className="player-option">
                <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`}/>
                <span className="player-option-label">{player.fullName}</span>
-           </Option>
+           </AAA>
        ));
        return (
            <AutoComplete
                className="search-bar"
-               dataSource={options}
+               dataSource={aaa}
                onSelect={this.onSelect}
                onSearch={this.handleSearch}
                placeholder="Search NBA Player"
